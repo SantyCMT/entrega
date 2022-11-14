f1da779911f7a9ab81edfc10c31baaad819d2f41
Santiago Ortiz <ortizsantiago90@gmail.com>
2022-11-14 00:20:31 -0300

    1.1

--- appmundial/urls.py
+++ appmundial/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="mundial-inicio"),
+    path("jugadores/",views.Jugadores, name="mundial-jugadores"),
+    path("directores_Tecn/",views.Directores_tecnicos, name="mundial-directores_Tecn"),
+    path("selecciones/",views.Selecciones, name="mundialselecciones"),
 ]
--- appmundial/views.py
+++ appmundial/views.py
@@ -3,4 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,"appmundial/index.html")
+    return render(request,"appmundial/base.html")
+
+
+def Jugadores(request):
+
+    return render(request, "appmundial/jugadores.html")
+
+def Directores_tecnicos(request):
+
+    return render(request, "appmundial/directores_tec.html")
+
+def Selecciones(request):
+
+    return render(request, "appmundial/selecciones.html")
