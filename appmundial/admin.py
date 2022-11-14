9773471987af1803044408ebdc0ce59ffb54d119
Santiago Ortiz <ortizsantiago90@gmail.com>
2022-11-14 00:59:58 -0300

    Update: admin

--- appmundial/admin.py
+++ appmundial/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from appmundial.models import *
 # Register your models here.
+
+admin.site.register(Jugadores)
+admin.site.register(selecciones)
+admin.site.register(DT)
